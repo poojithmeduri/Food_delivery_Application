06b09a1eadaac8b49b8c21412f8432891299e114
Poojith <poojithmeduri@gmail.com>
2024-07-31 17:54:42 +0530

    first commit

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import './ExploreMenu.css'
+import { StoreContext } from '../../Context/StoreContext'
+
+const ExploreMenu = ({category,setCategory}) => {
+
+  const {menu_list} = useContext(StoreContext);
+  
+  return (
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Craving something delicious? Get your favorite meals delivered hot and fresh right to your door! From gourmet delights to comfort classics, our speedy delivery ensures your cravings are satisfied in no time. Order now and savor the convenience and flavor!</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index)=>{
+            return (
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                    <img src={item.menu_image} className={category===item.menu_name?"active":""} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default ExploreMenu
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Crave, Click, Enjoy!</h2>
+                <p><b>Craving something delicious? Get your favorite meals delivered hot and fresh right to your door! From gourmet delights to comfort classics, our speedy delivery ensures your cravings are satisfied in no time. Order now and savor the convenience and flavor!</b></p>
+                {/* <button>View Menu</button> */}
+            </div>
+        </div>
+    )
+}
+
+export default Header
